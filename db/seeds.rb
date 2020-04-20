f9b1db974a25ec222efa6a19066b75bf3c207518
biagioo <mendolia2328@gmail.com>
2020-04-20 16:28:40 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+biagio = Artist.create!(name: "Biagio")
+biagio.songs.create!(title: "Coding is fun!")
+biagio.songs.create!(title: "Coding is hard!")
